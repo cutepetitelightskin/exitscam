5d62deda1a626e98038a32383dff6063206d4625
cutepetitelightskin <lolasmithh5922@gmail.com>
2025-03-15 11:14:28 +0000

    Update anything.lua

--- anything.lua
+++ anything.lua
@@ -1,6 +1,6 @@
 shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​u​t​f​i​t​D​i​s​p​a​t​c​h​e​s​I​X​P​2"] = {
     ["Main"] = {
-        ['Loader'] = { ['Key'] = ('5pub2bGVLPWc4BQFasAQWMAebRFyMm5F') },
+        ['Loader'] = { ['Key'] = ('c8xNDFc2beQCD3BnnbBPTVeDEUFjYLGB') },
     },
     
     ["Universal"] = {
